dd1f27ba5fb238334a402473310149f62417e8c6
edmariooliver <barrigacraft@hotmail.com>
2023-03-23 18:43:14 -0300

    refactor: early return in Middleware

--- src/Middleware.php
+++ src/Middleware.php
@@ -18,31 +18,23 @@ trait Middleware
     {
         $access = false;
 
-        if(!is_null($middlewares)) {
-            foreach($middlewares as $middleware) {
-                if (class_exists($middleware)) {
-                    $middlewareInstance = new $middleware();
-                    if(method_exists($middlewareInstance, "handle")) {
-                        $this->middlewareAccess = $middlewareInstance->handle();
-                        if($this->middlewareAccess) {
-                            $access = true;
-                        }else{
-                            $access = false;
-                        }
-                    }else{
-                        throw new RouterException($middleware." not implemented correctly");
-                    }
-                }else{
-                    throw new RouterException($middleware." does not exists");
-                }
+        if(is_null($middlewares)) {
+           return false; 
+        }
+
+        foreach($middlewares as $middleware) {
+
+            if (!class_exists($middleware)) {
+                throw new RouterException($middleware ." does not exists");
             }
 
-            if($access) {
-                $this->middlewareAccess = true;
-            }else{
-                $this->middlewareAccess = false;
+            $middlewareInstance = new $middleware();
+
+            if(!method_exists($middlewareInstance, "handle")) {
+                throw new RouterException($middleware ." not implemented correctly");
             }
-            return $this;
+
+            return $this->middlewareAccess = $middlewareInstance->handle();
         }
     }
 }
